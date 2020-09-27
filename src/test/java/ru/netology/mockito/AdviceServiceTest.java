772e97e9ec340d59c010d252779545ada8607b66
sserdyuk <sserdyuk@fun.co>
2020-09-27 17:01:44 +0300

    step 7, use Spy and argument captor for checking input arguments

--- src/test/java/ru/netology/mockito/AdviceServiceTest.java
+++ src/test/java/ru/netology/mockito/AdviceServiceTest.java
@@ -1,7 +1,8 @@
 package ru.netology.mockito;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 
 import java.util.Set;
 
@@ -9,6 +10,7 @@ import ru.netology.mockito.external.services.preference.Preference;
 import ru.netology.mockito.external.services.preference.PreferencesService;
 import ru.netology.mockito.external.services.weather.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -21,18 +23,17 @@ class AdviceServiceTest {
     @DisplayName("should get advice in bad weather")
     void testGetAdviceInBadWeather() {
         WeatherService weatherService = mock(WeatherService.class);
-        when(weatherService.currentWeather())
-            .thenReturn(Weather.STORMY);
+        when(weatherService.currentWeather()).thenReturn(Weather.STORMY);
 
         PreferencesService preferencesService = mock(PreferencesService.class);
-        when(preferencesService.get(any()))
-            .thenReturn(Set.of(Preference.FOOTBALL));
+        when(preferencesService.get(any())).thenReturn(Set.of(Preference.FOOTBALL));
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
 
         AdviceService adviceService = new AdviceService(preferencesService, weatherService);
         adviceService.getAdvice("user1");
 
-        verify(preferencesService, times(1)).get("user1");
-        verify(preferencesService, times(0)).get("user2");
+        verify(preferencesService).get(argumentCaptor.capture());
+        assertEquals("user1", argumentCaptor.getValue());
     }
 
     @Test
